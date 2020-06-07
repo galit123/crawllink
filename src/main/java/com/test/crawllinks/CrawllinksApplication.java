e6690600dbfb52fd0e2058b286e168137f8bcde9
Galit H <galithayat@gmail.com>
2020-06-07 18:50:54 +0300

    upload crawllink project

---
+++ src/main/java/com/test/crawllinks/CrawllinksApplication.java
@@ -0,0 +1,13 @@
+package com.test.crawllinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawllinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawllinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/crawllinks/CrawllinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.crawllinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawllinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
